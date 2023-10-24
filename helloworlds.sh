68afdb240013047702c4012ed69a10e44e45a56a
Scotty Jokon <Jokon.Scotty@gmail.com>
2023-10-24 16:17:38 -0700

    helloworld

---
+++ helloworlds.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# script name: Helloworld.sh
+# Author: Scotty Jokon
+# Date of latest revision: 10/24/24
+# Purpose: Ops-Challenge02
+
+# Declaration of variables
+
+# Declaration of functions
+
+# Main
+echo "hello world"
+
+
+# End
