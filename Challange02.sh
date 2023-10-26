6272a065d0aab56a49d2e9eb38449a120af84a61
Scotty Jokon <Jokon.Scotty@gmail.com>
2023-10-25 18:49:06 -0700

    Challange02

---
+++ Challange02.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Script Name:                  c2.sh
+# Author:                       Scotty Jokon
+# Date of latest revision:      10/25/2023
+# Purpose:                      Ops 201 Challange02
+
+# Declaration of variables 
+
+
+# Declaration of functions
+
+print_somthing() {
+echo $This is the login history
+}
+
+# Main
+
+print_somthing
+print_somthing
+print_somthing
+
+# End
