4969c249d1a8b21b52d2ccd1db69c3b05b5532c8
Scotty Jokon <Jokon.Scotty@gmail.com>
2023-10-25 17:19:51 -0700

    i am working perfectly

--- helloworlds.sh
+++ helloworlds.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# script name: Helloworld.sh
-# Author: Scotty Jokon
-# Date of latest revision: 10/24/24
-# Purpose: Ops-Challenge02
+# Script Name:                  Helloworld.sh
+# Author:                       Scotty Jokon
+# Date of latest revision:      10/24/24
+# Purpose:                      Ops-Challenge02
 
 # Declaration of variables
 
 # Declaration of functions
+
 
 # Main
 echo "hello world"
