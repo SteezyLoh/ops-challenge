defa157d38e76a383d00294e101910b6220a89e1
Scotty Jokon <Jokon.Scotty@gmail.com>
2023-10-26 20:49:54 -0700

    array update

---
+++ array.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Script Name:                  array.sh
+# Author:                       Scotty Jokon        
+# Date of latest revision:      10/26/2023
+# Purpose:                      demo array
+
+# Declaration of variables
+
+Cow="dir"
+#             index 0 index 1
+Chicken =("dir1","dir2","dir3","dir4")
+
+
+# Declaration of functions
+
+echo ${dir[0]}
+echo ${dir[2]}
+echo ${dir[3]}
+
+
+
+# Main
+
+
+
+# End
