5c99a2c9f9d7a9f3da9a5face4b8f90ba7337bb1
Scotty Jokon <Jokon.Scotty@gmail.com>
2023-10-28 18:12:12 -0700

    array.sh

--- array.sh
+++ array.sh
@@ -4,24 +4,29 @@
 # Author:                       Scotty Jokon        
 # Date of latest revision:      10/26/2023
 # Purpose:                      demo array
-
+# sources:                      Chat.openai.com
 # Declaration of variables
 
-Cow="dir"
-#             index 0 index 1
-Chicken =("dir1","dir2","dir3","dir4")
+#!/bin/bash
 
+mkdir -p dir1 dir2 dir3 dir4
+
+directories=("dir1" "dir2" "dir3" "dir4")
+
+for dir in "${directories[@]}"; do
+    touch "$dir/newfile.txt"
+done
+
+ls -R
 
 # Declaration of functions
 
-echo ${dir[0]}
-echo ${dir[2]}
-echo ${dir[3]}
+
 
 
 
 # Main
 
-
+echo $mikdr
 
 # End
